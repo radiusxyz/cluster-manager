db644a269a5d704a8166d8c863f0dfcc2f93a8f5
gylman <gylmanbm@gmail.com>
2024-08-20 16:41:59 +0900

    add router and main routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,36 @@
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
-// import TotalView from "./pages/TotalView";
-import TestContractFunctions from "./components/TestContractFunctions";
-import TestNonContractFunctions from "./components/TestNonContractFunctions";
-import Integration from "./components/Integration";
+import ExplorerPage from "./pages/ExplorerPage";
+import DashboardPage from "./pages/DashboardPage";
 
 const router = createBrowserRouter([
   {
-    // path: "/",
-    // element: <RootLayout />,
-    // children: [
-    //   {
-    //     index: true,
-    //     path: "/",
-    //     element: <TotalView />,
-    //     loader: () => {
-    //       window.scrollTo(0, 0);
-    //       return null;
-    //     },
-    //   },
-    // ],
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <ExplorerPage />,
+        loader: () => {
+          window.scrollTo(0, 0);
+          return null;
+        },
+      },
+      {
+        path: "dashboard", // No index: true here
+        element: <DashboardPage />,
+        loader: () => {
+          window.scrollTo(0, 0);
+          return null;
+        },
+      },
+    ],
   },
 ]);
 
 function App() {
-  // return <RouterProvider router={router} />;
-  return <Integration />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/ExplorerPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExplorerPage = () => {
+  return <div>ExplorerPage</div>;
+};
+
+export default ExplorerPage;
