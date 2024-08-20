e7e9fee63bba9921f34cf9f9dd6817d7916a2027
gylman <gylmanbm@gmail.com>
2024-08-20 18:05:42 +0900

    feat: add navigation bar, add footer, debug

--- client/src/pages/RootLayout.jsx
+++ client/src/pages/RootLayout.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import { Outlet } from "react-router";
+import styled from "styled-components";
+import Footer from "../components/Footer";
+
+const NavBarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LinksContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 50px;
+`;
+
+const Link = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Stat = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StatsContainer = styled(LinksContainer)``;
 
 const RootLayout = () => {
   return (
-    <div>
+    <>
+      <NavBarContainer>
+        <LinksContainer>
+          <Link>Dashboard</Link>
+          <Link>Explorer</Link>
+        </LinksContainer>
+        <StatsContainer>
+          <Stat>Total #</Stat>
+          <Stat>Total $</Stat>
+          <Stat>Wallet Address</Stat>
+        </StatsContainer>
+      </NavBarContainer>
       <Outlet />
-    </div>
+      <Footer />
+    </>
   );
 };
 
