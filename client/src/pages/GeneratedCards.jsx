922e70bcbb4062a0438619c4ee7ea92bbd3448d9
gylman <gylmanbm@gmail.com>
2024-08-22 09:28:41 +0900

    add generated cards components, and its structure

---
+++ client/src/pages/GeneratedCards.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GeneratedCards = () => {
+  return <div>GeneratedCards</div>;
+};
+
+export default GeneratedCards;
---
+++ client/src/pages/GeneratedCardsStyles.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const CardsContainer = styled.grid``;
+
+export const Card = styled.div``;
+
+export const TopPart = styled.div``;
+
+export const NameIdWrapper = styled.div``;
+
+export const Name = styled.span``;
+export const Id = styled.span``;
+
+export const EditBtn = styled.button``;
+
+export const BottomPart = styled.div``;
+
+export const BottomPropWrapper = styled.div``;
+
+export const PropTitle = styled.span``;
+export const PropValue = styled.span``;
