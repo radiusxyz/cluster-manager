0df93d58bee1ae355823c7cdbb9d27fc51aead94
gylman <gylmanbm@gmail.com>
2024-08-22 09:40:23 +0900

    add components, refactor

--- client/src/pages/GeneratedCardsStyles.jsx
+++ client/src/pages/GeneratedCardsStyles.jsx
@@ -4,7 +4,7 @@ export const CardsContainer = styled.grid``;
 
 export const Card = styled.div``;
 
-export const TopPart = styled.div``;
+export const NameIdEditWrapper = styled.div``;
 
 export const NameIdWrapper = styled.div``;
 
@@ -13,9 +13,9 @@ export const Id = styled.span``;
 
 export const EditBtn = styled.button``;
 
-export const BottomPart = styled.div``;
+export const PropsWrapper = styled.div``;
 
-export const BottomPropWrapper = styled.div``;
+export const PropWrapper = styled.div``;
 
 export const PropTitle = styled.span``;
 export const PropValue = styled.span``;
