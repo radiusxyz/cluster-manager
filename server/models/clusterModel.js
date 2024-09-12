e93707bb85c57566956fe6b77a1568a4670ee0c2
gylman <gylmanbm@gmail.com>
2024-09-12 12:14:59 +0900

    fix: back to objects

--- server/models/clusterModel.js
+++ server/models/clusterModel.js
@@ -7,32 +7,28 @@ const ValidationInfoSchema = new Schema({
 });
 
 const ExecutorSchema = new Schema({
+  address: { type: String, required: true },
   rpcUrl: { type: String, required: true },
   websocketUrl: { type: String, required: true },
   blockExplorerUrl: { type: String, required: true },
 });
 
 const RollupSchema = new Schema({
+  rollupId: { type: String, required: true },
   owner: { type: String, required: true },
   type: { type: String, required: true },
   orderCommitmentType: { type: String, required: true },
   validationInfo: { type: ValidationInfoSchema, required: true },
-  executors: {
-    type: Map,
-    of: ExecutorSchema,
-  },
+  executors: [ExecutorSchema],
 });
 
 const ClusterSchema = new Schema({
   clusterId: { type: String, required: true },
+  owner: { type: String, required: true },
   sequencers: [{ type: String }],
-  rollups: {
-    type: Map,
-    of: RollupSchema,
-  },
+  rollups: [RollupSchema],
   maxSequencerNumber: { type: Number, default: 0 },
   active: { type: Boolean, default: true },
-  createdAt: { type: Date, default: Date.now },
 });
 
 const Cluster = mongoose.model("Cluster", ClusterSchema);
