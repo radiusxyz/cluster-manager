0d5b4913adc405f1f00cbdada55ab7274f3308ff
gylman <gylmanbm@gmail.com>
2024-09-24 16:48:57 +0900

    feat: add default scripts and update config on rollup addition

--- server/services/clusterService.js
+++ server/services/clusterService.js
@@ -94,6 +94,37 @@ const addRollup = async (logs) => {
           serviceProvider: validationInfo.serviceProvider,
         },
         executors,
+        fileStrings: {
+          config: `# Set sequencer rpc url
+sequencer_rpc_url = "http://127.0.0.1:3000"
+
+# Set internal rpc url
+internal_rpc_url = "http://127.0.0.1:4000"
+
+# Set cluster rpc url
+cluster_rpc_url = "http://127.0.0.1:5000"
+
+# Set seeder rpc url
+seeder_rpc_url = "http://127.0.0.1:6001"
+
+# Set key management system rpc url
+key_management_system_rpc_url = "http://127.0.0.1:7100"
+
+# Set cluster type
+cluster_type = "local"
+
+# Set liveness provider rpc url
+liveness_provider_rpc_url = "http://127.0.0.1:8545"
+
+# Set liveness provider websocket url
+liveness_provider_websocket_url = "ws://127.0.0.1:8545"
+
+# Set liveness contract address
+liveness_contract_address = ""
+
+# Set using zkp
+is_using_zkp = false`,
+        },
       });
 
       await cluster.save();
