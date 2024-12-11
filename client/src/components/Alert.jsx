3f32f28502254aa4459ddaee59fcdf955a5edd62
gylman <gylmanbm@gmail.com>
2024-12-11 14:02:55 +0900

    fix: prop error

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Title, Message } from "./AlertStyles";
 
 const Alert = ({ error }) => (
-  <Container error={error}>
+  <Container $error={error}>
     <Title>{error ? "Error" : "Success"}</Title>
     <Message>{error ? error : "Succesfully wrote to contract"}</Message>
   </Container>
--- client/src/components/AlertStyles.jsx
+++ client/src/components/AlertStyles.jsx
@@ -11,18 +11,18 @@ export const Container = styled.div`
   margin: 20px auto; /* Center horizontally */
   border-radius: 5px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  background-color: ${({ error }) => (error ? "#ffe6e6" : "#e6ffe6")};
-  border: 1px solid ${({ error }) => (error ? "#ff5c5c" : "#5cff5c")};
+  background-color: ${({ $error }) => ($error ? "#ffe6e6" : "#e6ffe6")};
+  border: 1px solid ${({ $error }) => ($error ? "#ff5c5c" : "#5cff5c")};
 `;
 
 export const Title = styled.h3`
   font-size: 18px;
   font-weight: bold;
-  color: ${({ error }) => (error ? "#b30000" : "#006600")};
+  color: ${({ $error }) => ($error ? "#b30000" : "#006600")};
   margin-bottom: 5px;
 `;
 
 export const Message = styled.p`
   font-size: 16px;
-  color: ${({ error }) => (error ? "#800000" : "#004d00")};
+  color: ${({ $error }) => ($error ? "#800000" : "#004d00")};
 `;
